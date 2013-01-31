6e60301cf9dc2206a32a5cf94da98de5fb2de39a
Chris Cacciatore <chris.cacciatore@gmail.com>
2013-01-31 13:24:00 -0800

    debugging

--- lib/prog-rock/concert.rb
+++ lib/prog-rock/concert.rb
@@ -5,6 +5,8 @@ module ProgRock
     GLYPHS = {}
     class Concert
         def initialize
+		require 'pry'
+		binding.pry
             Dir.glob("arts-and-farts/*.json").each do |file|
 		glyph = JSON.parse File.open(file,'r').readlines.join
                 GLYPHS[glyph["name"].to_sym] = glyph["frames"]
